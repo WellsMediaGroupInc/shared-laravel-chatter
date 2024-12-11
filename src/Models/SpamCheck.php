b62609b4f1b4571017092f85e8532085332d0f9d
Jason Chipp <jchipp@wellsmedia.com>
2024-12-11 00:03:24 -0700

    use older laravel syntax for uuids

--- src/Models/SpamCheck.php
+++ src/Models/SpamCheck.php
@@ -3,12 +3,10 @@
 namespace DevDojo\Chatter\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Support\Str;
 
 class SpamCheck extends Model
 {
-    use HasUuids;
-
     protected $table = 'chatter_spam_checks';
     protected $guarded = ['id'];
     protected $casts = [
@@ -17,8 +15,29 @@ class SpamCheck extends Model
         'completed_at' => 'datetime',
     ];
 
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            if (empty($model->{$model->getKeyName()})) {
+                $model->{$model->getKeyName()} = (string) Str::uuid();
+            }
+        });
+    }
+
+    public function getIncrementing()
+    {
+        return false;
+    }
+
+    public function getKeyType()
+    {
+        return 'string';
+    }
+
     public function checkable()
     {
         return $this->morphTo();
     }
-} 
+}
